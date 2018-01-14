2b4dc264f863883987c5b4e7423a7130cb7a269d
adamwbowman <adamwbowman@gmail.com>
2018-01-13 23:06:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+'use strict';
+
+var express = require('express');
+var app = express();
+
+// Adding tabs to our app. This will setup routes to various views
+// var tabs = require('./tabs');
+// tabs.setup(app);
+
+// Adding a bot to our app
+var bot = require('./bot');
+bot.setup(app);
+
+// Adding a messaging extension to our app
+// var messagingExtension = require('./messaging-extension');
+// messagingExtension.setup();
+
+// Start our nodejs app
+app.listen(process.env.PORT || 3333, function() {
+	console.log('App started listening on port 3333');
+});
+
+
+
+
+// var http = require('http');
+
+// var server = http.createServer(function(req, res) {
+// res.writeHead(200);
+// res.end('Hi everybody!');
+// });
+// server.listen(3333);
